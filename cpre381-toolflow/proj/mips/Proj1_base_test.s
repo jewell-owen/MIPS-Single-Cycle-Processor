a61827eb20cd798c172bfc1eac86a2b1f95c9e4b
ojewell <ojewell@co2050-20.ece.iastate.edu>
2024-10-09 13:46:49 -0500

    added lw, sw and fixed possible issue with temp registers not persiting across a function call

--- cpre381-toolflow/proj/mips/Proj1_base_test.s
+++ cpre381-toolflow/proj/mips/Proj1_base_test.s
@@ -8,6 +8,10 @@
 
 # code/instruction section
 .text
+
+main:
+sw $31, 0($31)                  # Store return adress at adress of return address
+lw $31, 0($31)                  # Load return addrress from address of return address    
 addi  $8,  $0,    1 		# Load 1 in register 8        ($r8  = 0000 0001 [1] )
 addi  $9,  $0,    2		# Load 2 in register 9        ($r9  = 0000 0002 [2] )
 addi  $10, $0,    5		# Load 5 in register 10       ($r10 = 0000 0005 [5] )
@@ -23,9 +27,14 @@ jr $ra
 
 Next:
 sub   $17, $11,  17		# Load 3 - 7 in register 17   ($r17 = FFFF FFFC [-4])
-sll   $18, $17,   1             # Load 4 * 2 into register 18 ($r18 = 0000 0008 [8] )
+jal func                        # Call func function
 subu  $19, $11, $10		# Load 3 - 5 unsigned into 19 ($r19 = 0000 0002 [2] )
-jal ors                         # Call ors function
+or    $20, $17, $10             # Load -4 OR 7 in register 20 ($r20 = FFFF FFFF [-1])
+ori   $21, $8,    6             # Load 1 OR 6 in register 21  ($r21 = 0000 0007 [7] )
+xor   $22, $14, $15             # Load 7 XOR 6 in register 22 ($r22 = 0000 0001 [1] )
+sll   $23, $10,   1             # Load 5 * 2 into register 23 ($r23 = 0000 000A [10])
+xori  $8, $23,    7             # Load 10 XOR 7 in register 8 ($r8 = 0000 000D [13])
+nor   $9, $11,   14             # Load -4 NOR 6 in register 9 ($r9 = 0000 0001 [1] )                    
 srl   $10, $12,   4             # Load -3 shift right logic 4 ($r10 = 0FFF FFFF [268435455])
 sra   $11, $12,   4             # Load -3 shift right arith 4 ($r11 = FFFF FFFF [-1])
 slt   $14, $12,   $13           # Load -3 < 2 into register 14 ($r14 = 0000 0001 [1] )
@@ -33,18 +42,8 @@ slti  $15, $17,   0             # Load -3 < 0 into register 15($r15 = 0000 0001 
 lui   $16, 8                    # Load 8 into upper 8 bits    ($r16 = 0008 0000 [524288])
 j Finish                        # Go to finish
 
-ors:
-or    $20, $17, $10             # Load -4 OR 7 in register 20 ($r20 = FFFF FFFF [-1])
-ori   $21, $8,    6             # Load 1 OR 6 in register 21  ($r21 = 0000 0007 [7] )
-xor   $22, $14, $15             # Load 7 XOR 6 in register 22 ($r22 = 0000 0001 [1] )
-sll   $23, $10,   1             # Load 5 * 2 into register 23 ($r23 = 0000 000A [10])
-xori  $8, $23,    7             # Load 10 XOR 7 in register 8 ($r8 = 0000 000D [13])
-nor   $9, $11,   14             # Load -4 NOR 6 in register 9 ($r9 = 0000 0001 [1] )
-
-To-DO-------------------------------------------
-lw
-sw
-------------------------------------------------
+func:
+sll   $18, $17,   1             # Load 4 * 2 into register 18 ($r18 = 0000 0008 [8] )
 
 
 Reg2NEReg13:
