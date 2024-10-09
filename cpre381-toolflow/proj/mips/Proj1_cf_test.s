413407001ce75005660025674bf72aee5fa885d0
JasonDiGiovanni <jdigiovanni0727@gmail.com>
2024-10-09 13:28:18 -0500

    Finished Proj1_cf_test.s

--- cpre381-toolflow/proj/mips/Proj1_cf_test.s
+++ cpre381-toolflow/proj/mips/Proj1_cf_test.s
@@ -1,5 +1,8 @@
 #
-# First part of the Lab 3 test program
+# Test program for mips control flow instructions and has a call depth of at least 5 
+# beq, bne, j, jal, jr
+# The program should jump (in order) init, RUN2, RUN1, RUN4, RUN3, Finish RUN4 halt
+# There's a chance 3 and 4 loop forever
 #
 
 # data section
@@ -7,15 +10,27 @@
 
 # code/instruction section
 .text
-addi  $1,  $0,  1 		# Place â€œ1â€? in $1
-addi  $2,  $0,  2		# Place â€œ2â€? in $2
-addi  $3,  $0,  3		# Place â€œ3â€? in $3
-addi  $4,  $0,  4		# Place â€œ4â€? in $4
-addi  $5,  $0,  5		# Place â€œ5â€? in $5
-addi  $6,  $0,  6		# Place â€œ6â€? in $6
-addi  $7,  $0,  7		# Place â€œ7â€? in $7
-addi  $8,  $0,  8		# Place â€œ8â€? in $8
-addi  $9,  $0,  9		# Place â€œ9â€? in $9
-addi  $10, $0,  10		# Place â€œ10â€? in $10
+init:
+    addi  $1,  $0,  1 		# Place 1 in $1
+    j RUN2                  # Jump to RUN2
+RUN1
+    addi $1, $1, 1          # Add 1 to $1
+    addi $4, $0, 4          # Place 4 in $4
+    bne  $1, $4, RUN4       # Jump 
 
-halt
+RUN2
+    addi $1, $1, 1          # Add 1 to $1
+    addi $2, $0, 2          # Place 2 in $2
+    beq  $1, $2, RUN1       # If $1(2) and $2(2) are equal, jump to RUN1
+
+RUN3
+    addi $1, $1, 1          # Add 1 to $1
+    jr $ra                  # Jump to the return address $ra
+
+RUN4
+    addi $1, $1, 1          # Add 1 to $1
+    jal RUN3                # Jump and write return address to $ra
+    addi $1, $1, 5          # Add 5 to $1 after returning from RUN3
+
+halt:
+    lui $2, $0, 10         # This is after the exit comment in homework code, but it might be unnecessary 
