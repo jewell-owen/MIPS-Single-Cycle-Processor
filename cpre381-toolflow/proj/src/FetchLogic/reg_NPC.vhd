9301222b44dc59911184e658c211da21df1b9c01
Owen Jewell <jewell.owen@yahoo.com>
2024-10-23 12:47:19 -0500

    adding files before snyth

--- cpre381-toolflow/proj/src/FetchLogic/reg_NPC.vhd
+++ cpre381-toolflow/proj/src/FetchLogic/reg_NPC.vhd
@@ -18,7 +18,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity reg_N is
+entity reg_NPC is
   generic(N : integer := 32); 
 
   port(i_CLK        : in std_logic;     -- Clock input
@@ -27,9 +27,9 @@ entity reg_N is
        i_D          : in std_logic_vector(N-1 downto 0);     -- Data value inputs
        o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value outputs
 
-end reg_N;
+end reg_NPC;
 
-architecture structure of reg_N is
+architecture structure of reg_NPC is
 
 
   component dffg
@@ -52,15 +52,32 @@ architecture structure of reg_N is
 begin
 
 
-  G_NBit_Reg: for i in 0 to N-1 generate
+  G_NBit_Reg0: for i in 0 to 21 generate
     REGI: dffg port map(
 	      i_CLK     => i_CLK,
 	      i_RST     => i_RST,
 	      i_WE      => i_WE,
 	      i_D       => i_D(i),
 	      o_Q       => o_Q(i));
-  end generate G_NBit_Reg;
+  end generate G_NBit_Reg0;
 
+  G_NBit_Reg1: for i in 22 to 22 generate
+    REGI: dffgPC port map(
+	      i_CLK     => i_CLK,
+	      i_RST     => i_RST,
+	      i_WE      => i_WE,
+	      i_D       => i_D(i),
+	      o_Q       => o_Q(i));
+  end generate G_NBit_Reg1;
+
+  G_NBit_Reg2: for i in 23 to N-1 generate
+    REGI: dffg port map(
+	      i_CLK     => i_CLK,
+	      i_RST     => i_RST,
+	      i_WE      => i_WE,
+	      i_D       => i_D(i),
+	      o_Q       => o_Q(i));
+  end generate G_NBit_Reg2;
 
   
 end structure;
