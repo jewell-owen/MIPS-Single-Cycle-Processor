077888f658e2cf4f31facd43d4243b37091bbad8
Owen Jewell <jewell.owen@yahoo.com>
2024-11-14 16:28:01 -0600

    sw pipeline done for testing

--- cpre381-toolflow/proj/mips/Proj1_base_test.s
+++ cpre381-toolflow/proj/mips/Proj1_base_test.s
@@ -37,23 +37,18 @@ main:
     # Branch/Jump instructions
     beq $t1, $t2, skip      # t1 != t2, so no branch
     nop
-    nop
-    nop
+
     bne $t1, $t2, continue  # t1 != t2, so branch to continue
-    nop
-    nop
     nop
    
     
 skip:
     j end                   # Jump to end
     nop
-    nop
-    nop
+
 
 continue:
     jal dummy_function      # Jump and link to dummy_function
-    nop
     nop
     nop
     halt
@@ -61,10 +56,12 @@ continue:
 
 dummy_function:
     # Dummy function to demonstrate jal and jr usage
+    nop
+    nop
     jr $ra                  # Jump to the return address (continue execution at end)
     nop
-    nop
-    nop
+
+
 end:
   
 
